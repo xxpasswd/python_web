6c8cef5ab893f533eb8171ca0f26904be595a94d
xxpasswd <1490621113@qq.com>
2019-03-11 18:52:14 +0800

    更新文档

--- simple_web_server.py
+++ simple_web_server.py
@@ -1,21 +1,24 @@
+# coding: utf-8
+"""
+创建socket -----> 绑定监听地址 -------> 进行监听 ----->
+获取连接的socket -----> 服务端socket和客户端socket进行通信
+"""
 from socket import *
 
-HOST,PORT = '',8000
+HOST, PORT = '', 8000
 
-ss = socket(AF_INET,SOCK_STREAM)
+ss = socket(AF_INET, SOCK_STREAM)
 
 # Allow to resume same address
-ss.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-ss.bind((HOST,PORT))
+ss.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+ss.bind((HOST, PORT))
 ss.listen(1)
 print("Server HTTP on port {}".format(PORT))
 
 num = 0
 while True:
-    cs,addr = ss.accept()
+    cs, addr = ss.accept()
     request = cs.recv(1024)
-    num += 1
-    print (num)
     print(request.decode('utf-8'))
 
     http_response = b"""\
